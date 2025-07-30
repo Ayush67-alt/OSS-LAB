7e7a02122530e97f4e8162d0b56669234e1ca173
Ayush <23104031@mail.jiit.ac.in>
2025-07-30 10:49:11 +0530

    a

---
+++ index.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    int arr[100];
+    cout<<"Enter the size of array ";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cout<<"Enter element "<<i+1<<" ";
+        cin>>arr[i];
+    }
+    int sum=0;
+    for(int i=0;i<n;i++){
+        sum+=arr[i];
+        
+    }
+    int ans=0;
+    ans=sum/n;
+    cout<<"Average is "<<ans;
+
+}
